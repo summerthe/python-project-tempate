4509b969e72a9266dbaa00aee89b02437f58260b
The Summer <46783121+summerthe@users.noreply.github.com>
2022-10-25 17:00:01 +0530

    Update utils.py

--- src/project/utils.py
+++ src/project/utils.py
@@ -1,14 +1,15 @@
 from pathlib import Path
+from typing import Any
 
 from dotenv import dotenv_values
 
 
-def set_env() -> dict[str, any]:
+def set_env() -> dict[str, Any]:
     """Set environment variables from .env file.
 
     Returns
     -------
-    dict[str,any]
+    dict[str,Any]
     """
     BASE_DIR = Path().resolve().parent
     ENV_PATH = BASE_DIR / ".env"
